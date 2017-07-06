6956c179ae827b9eb924ae4f70f2ec8469405ce3
Pratik Deshmukh <pratik@prodevans.com>
2017-07-06 18:03:51 +0000

    Add files via upload

---
+++ Prime_number.py
@@ -0,0 +1,22 @@
+
+#Date:- 17th Jun 2017
+#Python Program to generate prime numbers upto n
+
+n=int(raw_input('Enter your limit : '))
+
+flag=True
+
+for i in range(2,n):
+    for x in range(2,i):
+        if i%x==0:
+            flag=True
+            break
+        else:
+            if x==i-1:
+                flag=False
+        if not flag:
+            print(i)
+            
+        
+            
+        
